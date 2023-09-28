63182b4654f3c397539ab1e79954a4f848fa6484
gregdolley <gregdolley@gmail.com>
2023-09-27 22:28:25 -0700

    Aspect ratio experimenting fixing (#1)
    Merge branch aspect_ratio_experimenting_fixing to master. The problem of output image's aspect ratio not matching the original image has been fixed in this merge.

--- ascii-art.py
+++ ascii-art.py
@@ -19,7 +19,7 @@ from PIL import Image, ImageDraw, ImageFont
 #--------------------------------------------------------------------------------------------------
 # GLOBALS
 #--------------------------------------------------------------------------------------------------
-ASCII_CHARS = ["#", "@", "%", "8", "9", "$", "7", "*", "?", "+", "-", ";", ":", ",", ".", " "]
+ASCII_CHARS = ["@", "%", "#", "8", "$", "9", "7", "?", "*", ";", "+", ":", "-", ",", ".", " "]
 ASCII_WHITE_INDEX = len(ASCII_CHARS)-1
 DEFAULT_OUTPUT_FILENAME = "ascii_image"
 
@@ -32,7 +32,9 @@ def file_exists(filename): return (os.path.isfile(filename) == True)
 def dir_exists(dir_name): return (os.path.isdir(dir_name) == True)
 def create_dir(dir_name): os.mkdir(dir_name)
 def convert_image_to_grayscale(image): return image.convert("L")
-def disable_antialiasing(renderer): renderer.fontmode = "1" # turns off anti-aliasing on TT fonts (renderer = ImageDraw object)
+def disable_antialiasing(renderer): renderer.fontmode = "1" # turns off anti-aliasing on TT fonts (renderer = ImageDraw object)    
+def get_mbox_width(font): return font.getbbox("M")[2] # using the "M" character glyph since traditionally font metrics are calculated based on the "M" box...
+def get_mbox_height(font): return font.getbbox("M")[3] # ...same here
 
 #--------------------------------------------------------------------------------------------------
 # main() function entry-point
@@ -98,15 +100,16 @@ def textfile_to_image(textfile_path):
 
 def generate_ascii_art(input_image):
     greyscale_image = convert_image_to_grayscale(input_image)
-    ascii_str = pixels_to_ascii_chars(greyscale_image)
-    img_width = greyscale_image.width
-    ascii_img_text = ""
+    return add_line_breaks(pixels_to_ascii_chars(greyscale_image), greyscale_image.width)
 
-    # add line breaks between each line of text
-    for i in range(0, len(ascii_str), img_width):
-        ascii_img_text += ascii_str[i:i+img_width] + "\n"
-    
-    return ascii_img_text
+
+def add_line_breaks(ascii_str, line_length):
+    ascii_str_with_breaks = ""
+
+    for i in range(0, len(ascii_str), line_length):
+        ascii_str_with_breaks += ascii_str[i:i+line_length] + "\n"
+
+    return ascii_str_with_breaks
 
 
 def get_output_filenames_with_ext(output_filename):
@@ -134,16 +137,16 @@ def prompt_for_image_file():
 
 
 def parse_command_line_arguments():
-    parser = argparse.ArgumentParser(description='Command-line example.')
+    parser = argparse.ArgumentParser(description='Takes a color image and redraws it using ASCII characters instead of pixels. It saves the ASCII art as a text file and PNG file containing the exact same text.')
     parser.add_argument('-o', action='store', dest='output',
                         metavar='FILE',
                         help='Use FILE as output file name (without extension) instead of default (ascii_image.txt/.png)')
     parser.add_argument('-f', action='store', dest='font_file',
                         metavar='FONT_FILE_PATH',
-                        help='Use font file FONT_FILE_PATH for ASCII art font instead of default')
+                        help='Use font file FONT_FILE_PATH for ASCII art font instead of default (must be a monospaced font)')
     parser.add_argument('-w', action='store', dest='resize_image_width',
                         metavar='RESIZE_IMAGE_PIXEL_WIDTH',
-                        help='Resize the input image width to RESIZE_IMAGE_PIXEL_WIDTH pixels before converting to ASCII art (allows you to shrink/grow the input image if it is too big/small)')
+                        help='Resize the input image width to RESIZE_IMAGE_PIXEL_WIDTH pixels before converting to ASCII art (allows you to grow/shrink the input image if it is too big/small)')
 
     return parser.parse_args()
 
@@ -155,7 +158,6 @@ def get_user_config():
     new_image_width = int(args.resize_image_width, base=10) if "resize_image_width" in args and args.resize_image_width != None else image.width
     output_filename = args.output if "output" in args and args.output != None else DEFAULT_OUTPUT_FILENAME
     font_file = args.font_file if "font_file" in args and args.font_file!= None else ""
-    print(f"FONT FILE: {font_file}")
 
     return image, new_image_width, output_filename, font_file
 
@@ -181,34 +183,27 @@ def resize(image, new_width):
     image_aspect_ratio = image.height / image.width
     new_height = new_width * image_aspect_ratio # calc new height in pixels based on user specified width (preserving the original image's aspect ratio)
 
-    # The previous line calculated a new height for the input image because the user specified a custom image width (to preserve the original aspect ratio,
-    # obviously the height needs to be adjusted accordingly). The line below this comment block also calculates a new image height, but it's doing so in 
-    # order to compensate for the ASCII glyph shape not being a perfect square (I think all ASCII chars have more vertical pixels than horizontal ones for 
-    # all common latin fonts). So if you didn't adjust the height to take into account that each ASCII "pixel" in the output "ASCII image" is really a 
-    # vertical rectangle instead of a perfect square, then you would end up with an image that looked stretched vertically instead of maintaining the 
-    # original image's aspect ratio. The next couple paragraphs explain my solution to fix this because I don't think it's obvious from the code.
-    # If you expanded the formula right below this comment block, it would look like this:
+    # The previous line calculated a new height for the input image if new_width != image.width in order to preserve its aspect ratio.
+    # (If new_width == image.width, then the image.width divisor is cancelled out leaving just, "new_height = image.height", the original image height.)
+    # 
+    # The line below this comment block also calculates a new image height, but it's doing so in order to compensate for the ASCII glyph shape being a
+    # rectangle (where height > width) instead of a perfect square. If you didn't do this, then you would end up with ASCII images that look stretched 
+    # vertically. The next couple paragraphs explain my solution to fix this because it's not obvious from the code.
+    # 
+    # If you expanded the formula immediately below this comment block, it would look like this:
     #
     # adjusted_height  = 1 / (font_glyph_height / font_glyph_width) * new_height
     #
     # Basically what this formula is saying is: take the height of a glyph and express it as a percentage of its width (for example, if the height
-    # is 30, and the width is 15, then the height as a percentage of the width would be 200% (or 2.0; because 30 is 15 (width) * 2.0 = 30, or 15*200% = 30).
-    # Then take that number and innvert it (1 / 2.0 = 0.5 or 50%): the result is the percentage that you take from the image height to get the new
-    # and reduceed image height that will fit the vertical rectangular glyphs resulting in vertically stretching this "pre-reduced" image height back
-    # into the original height we wanted in the first place.
+    # is 30, and the width is 15, then the height as a percentage of the width would be 200% (2.0; because the width of 15 * 2.0 is 30). Then take that
+    # number and innvert it (1 / 2.0 = 0.5 or 50%): the result is the percentage of the original height that must be used as the new height (that's why
+    # the last operation  part of that formula is: "<inverted % result>*new_height").
     # 
     # Complete example: take a 200x200 image where the font glyphs are 15x30 (width 15, height 30), like in the previous example, and calculate the 
     # new height: 1/(30/15)*200 = 1/2*200 = 0.5*200 = 100 pixels high. So the input image is converted from 200x200 into 200x100, which is totally 
-    # the incorrect aspect ratio, BUT, if you render 200 15-pixel-wide glyphs, and 100 30-pixel-high glyphs, the resulting image dimensions in pixels 
-    # will be exactly 200*15 = 3,000 pixels wide and 100*30 = 3000 pixels high; in other words, a new image with the dimensions of 3000x3000 pixels, 
-    # which is a 1.0 aspect ratio image, the same as the original 200x200 input image (200/200 = 1.0).
-    #
-    # FIXME: the above explanation should work like that in theory, and I don't see anything wrong with it. BUT, what's _really_ happening when you
-    # run this script is it's generating output images which have aspect ratios that are about 3% off from their original images (it's barely noicable
-    # unless you look really closely at the original image and the output image). I think the reason for this slightly incorrect behavior lies in the
-    # vertical pixel spacing _between_ each ASCII line in the output image. If you zoom in on any set of two or three lines, you can tell there's some
-    # built-in vertical spacing between them, about three pixels or so. Update: I think I just figured out what might be causing this. This file is a 
-    # WIP until I confirm my theory is true and fix the issue.
+    # the incorrect aspect ratio (when using square pixels), BUT, if you render 200 15-pixel-wide glyphs, and 100 30-pixel-high glyphs, the resulting
+    # image dimensions in pixels will be exactly 200*15 = 3,000 pixels wide and 100*30 = 3000 pixels high; in other words, a new image with the
+    # dimensions of 3000x3000 pixels, matching the aspect ratio of the original image (3000/3000 = 200/200 = 1.0).
     new_height =  1/font_aspect_ratio*new_height
 
     return image.resize((new_width.__round__(), new_height.__round__()))
@@ -254,23 +249,16 @@ def calc_widest_line_in_pixels(lines, font):
     return int(ceil(max_line_width))
 
 
-def calc_tallest_line_in_pixels(lines, font):
-    tallest_line = max(lines, key=lambda line: font.getbbox(line)[3])
-    # took out numer of vertical pixels in the font's descent size from the calculation (testing a theory for more accurate aspect ratio's on the output image)
-    return font.getbbox(tallest_line)[3]#+font.getmetrics()[1] # tallest_line + font descent size in # y-pixels
-
-
 def sum_pixel_height_for_all_lines(lines, font):
-    max_line_height = calc_tallest_line_in_pixels(lines, font)
-    return int(ceil(max_line_height * len(lines))), max_line_height
+    single_line_height = get_mbox_height(font) 
+    return int(ceil(single_line_height * len(lines))), single_line_height
 
 
 def calc_font_aspect_ratio():
     font = get_font_object(False)
 
-    # using the "M" character glyph since traditionally font metrics are calculated based on the "M" box
-    Mbox_width = font.getbbox("M")[2]
-    Mbox_height = font.getbbox("M")[3]
+    Mbox_width = get_mbox_width(font)
+    Mbox_height = get_mbox_height(font)
 
     return Mbox_height / Mbox_width
 
