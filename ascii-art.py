2b605a47bb6f5081954f91f574643e868f59aa05
Greg Dolley <gregdolley@gmail.com>
2023-11-05 14:36:25 -0800

    Fixed all pylint warnings/errors

--- ascii-art.py
+++ ascii-art.py
@@ -9,7 +9,6 @@
 #---------------------------------------------------------------------------------------------
 import argparse
 import os
-import platform
 import sys
 import traceback
 from math import ceil
@@ -17,6 +16,7 @@ from math import ceil
 from PIL import Image, ImageDraw, ImageFont
 
 # pylint: disable=line-too-long, missing-function-docstring
+# pylint: disable=multiple-statements
 
 #--------------------------------------------------------------------------------------------------
 # GLOBALS
@@ -31,16 +31,13 @@ user_font_file = "" # pylint: disable=invalid-name
 #--------------------------------------------------------------------------------------------------
 # HELPER FUNCTIONS
 #--------------------------------------------------------------------------------------------------
-# pylint: disable=C0321
 def file_exists(filename): return os.path.isfile(filename) is True
 def dir_exists(dir_name): return os.path.isdir(dir_name) is True
 def create_dir(dir_name): os.mkdir(dir_name)
 def convert_image_to_grayscale(image): return image.convert("L")
-def disable_antialiasing(renderer): renderer.fontmode = "1" # turns off anti-aliasing on TT fonts (renderer = ImageDraw object)    
+def disable_antialiasing(renderer): renderer.fontmode = "1" # turns off anti-aliasing on TT fonts (renderer = ImageDraw object)
 def get_mbox_width(font): return font.getbbox("M")[2] # using the "M" character glyph since traditionally font metrics are calculated based on the "M" box...
 def get_mbox_height(font): return font.getbbox("M")[3] # ...same here
-
-# pylint: enable=C0321
 
 #--------------------------------------------------------------------------------------------------
 # main() function entry-point
@@ -67,6 +64,7 @@ def main():
 #--------------------------------------------------------------------------------------------------
 # SUPPORT FUNCTIONS
 #--------------------------------------------------------------------------------------------------
+# pylint: disable=multiple-statements, invalid-name
 def create_text_file(output_text_filename, text_content):
     dir_name = os.path.dirname(output_text_filename)
     if dir_name != '' and not dir_exists(dir_name): create_dir(dir_name)
@@ -79,7 +77,8 @@ def get_font_object(show_warnings=True):
     else:
         font_filename = get_monospace_font_filename()
 
-    if show_warnings is True and len(user_font_file) > 0 and not file_exists(user_font_file): print(f'Warning: could not find user-specified font file: "{user_font_file}"; using alternate font: "{font_filename}".')
+    if show_warnings is True and len(user_font_file) > 0 and not file_exists(user_font_file):
+        print(f'Warning: could not find user-specified font file: "{user_font_file}"; using alternate font: "{font_filename}".')
 
     try:
         font = ImageFont.truetype(font_filename, size=12, encoding='unic')
@@ -162,12 +161,12 @@ def parse_command_line_arguments():
 
 def get_user_config():
     args = parse_command_line_arguments()
-    input_filename = args.input if "input" in args and args.input != None else ""
+    input_filename = args.input if "input" in args and args.input is not None else ""
     path = prompt_for_image_file() if input_filename == "" else input_filename
     image = Image.open(path)
-    new_image_width = int(args.resize_image_width, base=10) if "resize_image_width" in args and args.resize_image_width != None else image.width
-    output_filename = args.output if "output" in args and args.output != None else DEFAULT_OUTPUT_FILENAME
-    font_file = args.font_file if "font_file" in args and args.font_file!= None else ""
+    new_image_width = int(args.resize_image_width, base=10) if "resize_image_width" in args and args.resize_image_width is not None else image.width
+    output_filename = args.output if "output" in args and args.output is not None else DEFAULT_OUTPUT_FILENAME
+    font_file = args.font_file if "font_file" in args and args.font_file is not None else ""
 
     return image, new_image_width, output_filename, font_file, input_filename
 
@@ -178,6 +177,7 @@ def pixels_to_ascii_chars(image):
     divisor = 255//(len(ASCII_CHARS)-2)+1
 
     for pixel in pixels:
+        # pylint: disable=fixme
         # TODO: it would be better if this was a non-linear scale based on the grayscale histogram of the image.
         # Some images get converted and end up too light
         ascii_char = ASCII_CHARS[pixel//divisor] if pixel != 255 else ASCII_CHARS[ASCII_WHITE_INDEX]
@@ -186,20 +186,20 @@ def pixels_to_ascii_chars(image):
 
 
 def resize(image, new_width):
-    if new_width == None: return image
+    if new_width is None: return image
     if new_width <= 0: return image
-    
+
     font_aspect_ratio = calc_font_aspect_ratio()
     image_aspect_ratio = image.height / image.width
     new_height = new_width * image_aspect_ratio # calc new height in pixels based on user specified width (preserving the original image's aspect ratio)
 
     # The previous line calculated a new height for the input image if new_width != image.width in order to preserve its aspect ratio.
     # (If new_width == image.width, then the image.width divisor is cancelled out leaving just, "new_height = image.height", the original image height.)
-    # 
+    #
     # The line below this comment block also calculates a new image height, but it's doing so in order to compensate for the ASCII glyph shape being a
-    # rectangle (where height > width) instead of a perfect square. If you didn't do this, then you would end up with ASCII images that look stretched 
+    # rectangle (where height > width) instead of a perfect square. If you didn't do this, then you would end up with ASCII images that look stretched
     # vertically. The next couple paragraphs explain my solution to fix this because it's not obvious from the code.
-    # 
+    #
     # If you expanded the formula immediately below this comment block, it would look like this:
     #
     # adjusted_height  = 1 / (font_glyph_height / font_glyph_width) * new_height
@@ -208,16 +208,15 @@ def resize(image, new_width):
     # is 30, and the width is 15, then the height as a percentage of the width would be 200% (2.0; because the width of 15 * 2.0 is 30). Then take that
     # number and innvert it (1 / 2.0 = 0.5 or 50%): the result is the percentage of the original height that must be used as the new height (that's why
     # the last operation  part of that formula is: "<inverted % result>*new_height").
-    # 
-    # Complete example: take a 200x200 image where the font glyphs are 15x30 (width 15, height 30), like in the previous example, and calculate the 
-    # new height: 1/(30/15)*200 = 1/2*200 = 0.5*200 = 100 pixels high. So the input image is converted from 200x200 into 200x100, which is totally 
+    #
+    # Complete example: take a 200x200 image where the font glyphs are 15x30 (width 15, height 30), like in the previous example, and calculate the
+    # new height: 1/(30/15)*200 = 1/2*200 = 0.5*200 = 100 pixels high. So the input image is converted from 200x200 into 200x100, which is totally
     # the incorrect aspect ratio (when using square pixels), BUT, if you render 200 15-pixel-wide glyphs, and 100 30-pixel-high glyphs, the resulting
     # image dimensions in pixels will be exactly 200*15 = 3,000 pixels wide and 100*30 = 3000 pixels high; in other words, a new image with the
     # dimensions of 3000x3000 pixels, matching the aspect ratio of the original image (3000/3000 = 200/200 = 1.0).
     new_height =  1/font_aspect_ratio*new_height
 
-    return image.resize((new_width.__round__(), new_height.__round__()))
-
+    return image.resize((round(new_width), round(new_height)))
 
 def get_monospace_font_filename():
     font_dirs = []
@@ -252,7 +251,7 @@ def create_grayscale_image_with_renderer(image_width, image_height):
     # Remember that grayscale images don't have RGB pixel values - they have
     # a single color channel going from 0 (black) to 255 (white) and all shades
     # of gray in between (from darkest gray of 1 to lightest gray of 254).
-    # In Image.new() below, "L" is for a 256-color grayscale palette and 
+    # In Image.new() below, "L" is for a 256-color grayscale palette and
     # "color=255" specifies the default background color should be white.
     image = Image.new("L", (image_width, image_height), color=255)
     return image, ImageDraw.Draw(image)
@@ -271,7 +270,7 @@ def calc_widest_line_in_pixels(lines, font):
 
 
 def sum_pixel_height_for_all_lines(lines, font):
-    single_line_height = get_mbox_height(font) 
+    single_line_height = get_mbox_height(font)
     return int(ceil(single_line_height * len(lines))), single_line_height
 
 
@@ -288,10 +287,10 @@ def callstack_prompt(from_traceback):
     while True:
         resp = input("Show callstack? (y/n) ").strip()
         if(len(resp) == 0 or resp == "n"): break
-        elif(resp == "y"): 
-            traceback.print_tb(from_traceback) 
+        elif resp == "y":
+            traceback.print_tb(from_traceback)
             break
-        
+
         print("Invalid input. Please try again (note: response is case-sensitive).")
 
 
@@ -301,9 +300,9 @@ if __name__ == '__main__': # prevent this file from getting "module" loaded
 
         print("Done.")
         print(f"Exit status code:  {status}")
-        
+
         sys.exit(status)
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         print(sys.exc_info()[1])
         callstack_prompt(sys.exc_info()[2])
         sys.exit(-1)
